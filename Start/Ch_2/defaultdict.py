0246ca9ef3e87483059ed10151ed18d2959f63f5
Alii <ahmed.ali.emam@gmail.com>
2024-12-02 23:56:23 +0000

    chapter 1 and chapter 2 answers

--- Start/Ch_2/defaultdict.py
+++ Start/Ch_2/defaultdict.py
@@ -7,11 +7,21 @@ from collections import defaultdict
 fruits = ['apple', 'pear', 'orange', 'banana',
           'apple', 'grape', 'banana', 'banana']
 
-# TODO: use a dictionary to count each element
+# use a dictionary to count each element
 fruitCounter = dict()
 
-# TODO: Count the elements in the list
+# Count the elements in the list
 for fruit in fruits:
-    fruitCounter[fruit] += 1
+    if fruit in fruitCounter.keys():
+        fruitCounter[fruit] += 1
+    else:
+        fruitCounter[fruit] = 1
 
-# TODO: print the result
+# the above is the normal way, the else condition will be executed one time.
+defaultCounter = defaultdict(int) # or could be defaultdict(lambda: 100) to start counting from 100
+for fruit in fruits:
+    defaultCounter[fruit] += 1
+
+# print the result
+print(fruitCounter)
+print(defaultCounter)
